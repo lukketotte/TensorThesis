41aa1b195636c878a9f4876558b8b0774c01d6b2
Lukas Arnroth <lukas.arnroth@gmail.com>
2018-02-13 13:41:33 +0100

    wokring on __init__ method

--- tucker.py
+++ tucker.py
@@ -9,8 +9,35 @@ class TuckerDecomposition():
     
     Kolda, Tamara Gibson. Multilinear operators for higher-order decompositions.
     United States Department of Eneregy, 2006
+
+    Parameters
+    ----------
+    data: data tensor of tf or np type
+    
+    ranks: desired rank of core tensor
+    
+    stop_thresh: thresh hold for stopping the algorithm
+    
+    epochs: maximum number of iterations?
+    
+    dtype: type of numbers in the tensor
+    
+    init: initialize the factor matricies
+
     """
 
-    def __init__(self, shape, ranks, regularize = 1e-5, 
-    	         dtype = tf.float64, init = 'random'):
-        
+    def __init__(self, X_data, ranks, stop_thresh = 1e-10, 
+    	         dtype = "float64", init = 'hosvd', epochs = 1000):
+
+        self.X = tf.Variable(X_data, dtype = dtype)
+        self.ranks = ranks
+        self.stop_thresh = stop_thresh
+        self.init = init
+        self.epochs = epochs
+        self.init_components(init, X_data)
+        self.component_matricies = []
+
+    def init_components(self, init, X_data): 
+
+
+
