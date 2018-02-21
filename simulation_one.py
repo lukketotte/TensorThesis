7319451efdf33c7ca678d1a1b20883f804ca3f5e
Lukas Arnroth <lukas.arnroth@gmail.com>
2018-02-21 15:36:52 +0100

    testing simulation scheme

--- simulation_one.py
+++ simulation_one.py
@@ -9,10 +9,22 @@ from scipy.linalg import eigh
 from parafac_als import parafac as pf
 from tucker_als import TuckerDecomposition as td
 
-# generate X from N(0,1) of size 5, 30, 30 (in python terms)
-X = random.normal(loc = 0, scale = 5, size = (20, 20 ,20))
+# generate X from N(0,1) of size 20, 20, 20 (in python terms)
+X = random.normal(loc = 0, scale = 1, size = (20, 20 ,20))
 
-max_R = 5
+tucker = td()
+tucker.rank = [10,10,10]
+tucker.X_data = X
+G = tucker.tucker_ALS()
+# get core tensor, all dimensions reduced to 50% rounded up
+G = refold_tf(G, [10,10,10], 0)
+
+init_op = tf.global_variables_initializer()
+with tf.Session() as sess:
+	sess.run(init_op)
+	G_np = G.eval()
+
+max_R = 10
 for i in range(max_R - 1):
 	pf_test = pf(init = "random", row_info = "original")
 	pf_test.X_data = X
@@ -22,21 +34,6 @@ for i in range(max_R - 1):
 	pf_test.parafac_ALS()
 
 	x_est = pf_test.reconstruct_X_data()
-
-	# print(X[0, :, :])
-
-	tucker = td()
-	tucker.rank = [15,15,15]
-	tucker.X_data = X
-	G = tucker.tucker_ALS()
-	# get core tensor, all dimensions reduced to 50% rounded up
-	G = refold_tf(G, [15,15,15], 0)
-
-	init_op = tf.global_variables_initializer()
-	with tf.Session() as sess:
-		sess.run(init_op)
-		print(x_est.eval())
-		G_np = G.eval()
 
 	tf.reset_default_graph()
 	
