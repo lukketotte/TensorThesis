4a2614e1843a73bbe65de4063762925e24cbf938
Lukas Arnroth <lukas.arnroth@gmail.com>
2018-02-21 12:02:47 +0100

    testing some more simulations

---
+++ simulation_one.py
@@ -0,0 +1,50 @@
+# This is a first simulation test
+# creating a tensor of size 30 x 30 x 5 from N(0,1)
+
+from utils import *
+import numpy as np
+from numpy import random as random
+import tensorflow as tf
+from scipy.linalg import eigh
+from parafac_als import parafac as pf
+from tucker_als import TuckerDecomposition as td
+
+# generate X from N(0,1) of size 5, 30, 30 (in python terms)
+X = random.normal(loc = 0, scale = 1, size = (5, 30 ,30))
+
+max_R = 10
+for i in range(max_R - 1):
+	pf_test = pf(init = "random", row_info = "original")
+	pf_test.X_data = X
+	pf_test.rank = i + 1
+	pf_test.init_factors()
+	# pf_test.row_info("original")
+	pf_test.parafac_ALS()
+
+	x_est = pf_test.reconstruct_X_data()
+
+	# print(X[0, :, :])
+
+	tucker = td()
+	tucker.rank = [3,15,15]
+	tucker.X_data = X
+	G = tucker.tucker_ALS()
+	# get core tensor, all dimensions reduced to 50% rounded up
+	G = refold_tf(G, [3,15,15], 0)
+
+	init_op = tf.global_variables_initializer()
+	with tf.Session() as sess:
+		sess.run(init_op)
+		print(x_est.eval())
+		G_np = G.eval()
+
+	tf.reset_default_graph()
+	
+	pf_core = pf(init = "random", row_info = "core")
+	pf_core.X_data = G_np
+	pf_core.rank = i + 1
+	pf_core.init_factors()
+	# pf_test.row_info("core")
+	pf_core.parafac_ALS()
+
+	tf.reset_default_graph()
